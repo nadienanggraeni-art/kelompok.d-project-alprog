0b77210fa976cba161d8d02dbb1466f085d16cda
KHALISA IRFANI 1 <khalisa.irfani@mhs.unsoed.ac.id>
2026-06-17 17:40:44 +0700

    Update | |-- 02_string_kalimat.R

--- |-- src_R/| |-- 02_string_kalimat.R
+++ |-- src_R/| |-- 02_string_kalimat.R
@@ -1,1 +1,14 @@
+K1 <- "Saya tak 'kan menyerah."
+K2 <- 'Ia berkata, "Aku menyayangimu."'
+K3 <-"\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+K4 <-"Surat keputusan itu bernomor 62/UN.34/19/2023."
+
+cat(K1,"\n")
+## Saya tak 'kan menyerah.
+cat(K2,"\n")
+## Ia berkata, "Aku menyayangimu."
+cat(K3,"\n")
+## "Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!"
+cat(K4,"\n")
+## Surat keputusan itu bernomor 62/UN.34/19/2023.
 
