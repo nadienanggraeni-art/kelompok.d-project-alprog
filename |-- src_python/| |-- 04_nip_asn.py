93fbe1823bacc8ebeb22f39af082c21a2e7a49bb
meyluna <meylunapurbaa@gmail.com>
2026-06-18 00:29:12 +0700

    Update | |-- 04_nip_asn.py

--- |-- src_python/| |-- 04_nip_asn.py
+++ |-- src_python/| |-- 04_nip_asn.py
@@ -40,6 +40,51 @@ def tampilkan_tanggal_lahir(nip):
 tampilkan_tanggal_lahir("199301212019031010")
 
 
+#Kondisi Normal
+def tampilkan_tanggal_lahir(nip):
+    if len(nip) < 8:
+        print("NIP tidak valid")
+    else:
+        tahun = nip[0:4]
+        tanggal = nip[4:6]
+        bulan = nip[6:8]
+
+        if bulan == "01":
+            nama_bulan = "Januari"
+        elif bulan == "02":
+            nama_bulan = "Februari"
+        elif bulan == "03":
+            nama_bulan = "Maret"
+        elif bulan == "04":
+            nama_bulan = "April"
+        elif bulan == "05":
+            nama_bulan = "Mei"
+        elif bulan == "06":
+            nama_bulan = "Juni"
+        elif bulan == "07":
+            nama_bulan = "Juli"
+        elif bulan == "08":
+            nama_bulan = "Agustus"
+        elif bulan == "09":
+            nama_bulan = "September"
+        elif bulan == "10":
+            nama_bulan = "Oktober"
+        elif bulan == "11":
+            nama_bulan = "November"
+        elif bulan == "12":
+            nama_bulan = "Desember"
+        else:
+            nama_bulan = "Bulan tidak valid"
+
+        print("Tanggal Lahir ASN:", tanggal, nama_bulan, tahun)
+
+
+# Memanggil fungsi
+tampilkan_tanggal_lahir("200707052024012002")
+
+
+
+
 
 
 
