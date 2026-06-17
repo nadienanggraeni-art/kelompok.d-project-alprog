54d23c7e1f4ed21cd6f4a1f6185a1cbe54e1542e
NADIEN DWI ANGGRAENI 1 <nadien.anggraeni@mhs.unsoed.ac.id>
2026-06-17 15:41:32 +0700

    Update | |-- 01_analisis_teks.py

--- |-- src_python/| |-- 01_analisis_teks.py
+++ |-- src_python/| |-- 01_analisis_teks.py
@@ -1,1 +1,14 @@
+# Program Menghitung Jumlah Kata dan Jumlah Kalimat
 
+teks = input("Masukkan teks: ")
+
+if teks.strip() == "":
+    jumlah_kata = 0
+    jumlah_kalimat = 0
+else:
+    jumlah_kata = len(teks.split())
+    jumlah_kalimat = len(teks.split("."))-1
+
+
+print("\nTeks tersebut memuat", jumlah_kalimat,
+      "kalimat dan", jumlah_kata, "kata.")
