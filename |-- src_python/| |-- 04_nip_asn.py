5e47f203c839d55da220b2f6ccfa7157fcdf553e
meyluna <meylunapurbaa@gmail.com>
2026-06-18 00:45:24 +0700

    Update | |-- 04_nip_asn.py

--- |-- src_python/| |-- 04_nip_asn.py
+++ |-- src_python/| |-- 04_nip_asn.py
@@ -129,3 +129,52 @@ tampilkan_tanggal_lahir("20070705")
 
 
 
+#Kondisi Khusus           #tanggal tidak valid
+def tampilkan_tanggal_lahir(nip):
+    if len(nip) != 18:
+        print("Error: NIP harus berjumlah tepat 18 digit.")
+        return
+
+    tahun = nip[0:4]
+    bulan = nip[4:6]
+    tanggal = nip[6:8]
+
+    if bulan == "01":
+        nama_bulan = "Januari"
+    elif bulan == "02":
+        nama_bulan = "Februari"
+    elif bulan == "03":
+        nama_bulan = "Maret"
+    elif bulan == "04":
+        nama_bulan = "April"
+    elif bulan == "05":
+        nama_bulan = "Mei"
+    elif bulan == "06":
+        nama_bulan = "Juni"
+    elif bulan == "07":
+        nama_bulan = "Juli"
+    elif bulan == "08":
+        nama_bulan = "Agustus"
+    elif bulan == "09":
+        nama_bulan = "September"
+    elif bulan == "10":
+        nama_bulan = "Oktober"
+    elif bulan == "11":
+        nama_bulan = "November"
+    elif bulan == "12":
+        nama_bulan = "Desember"
+    else:
+        print("Error: Format bulan tidak valid (01-12).")
+        return
+
+    if not (tanggal.isdigit() and 1 <= int(tanggal) <= 31):
+        print("Error: Format tanggal tidak valid (01-31).")
+        return
+
+    print("Tanggal Lahir ASN:", tanggal, nama_bulan, tahun)
+
+
+# Memanggil fungsi
+tampilkan_tanggal_lahir("200705352019031010")
+
+
